64f1282ec1dbcab82f322943e6e14cc23dd5d580
stephenrains <applerains1234@gmail.com>
2025-12-13 23:44:08 -0800

    Updated navbar import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import NavbarComponent from "@/pages/components/NavbarComponent";
+import NavbarComponent from "@/components/NavbarComponent";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
